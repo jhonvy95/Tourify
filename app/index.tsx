19f52f6245051921599b153d725c40fe4b30e343
jhon varon <jhonvaronyate@hotmail.com>
2024-07-09 11:10:41 -0500

    setup and configuration

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          fontFamily: "outfit-medium",
+        }}
+      >
+        it worked
+      </Text>
+    </View>
+  );
+}
