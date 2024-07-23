bdb5f3f8316490d4d25b5fde5ef23c9e5202d014
jhon varon <jhonvaronyate@hotmail.com>
2024-07-22 22:12:43 -0500

    create select Travel date view

--- app/create-trip/select-dates.tsx
+++ app/create-trip/select-dates.tsx
@@ -1,10 +1,102 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity, ToastAndroid } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigation, useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import CalendarPicker from "react-native-calendar-picker";
+import moment, { Moment } from "moment";
+import { CreateTripContext } from "@/context/CreateTripContext";
 
 export default function SelectDates() {
+  const navigation = useNavigation();
+  const router = useRouter();
+  const { tripData, setTripData } = useContext(CreateTripContext);
+
+  const [startDate, setStartDate] = useState<Moment | null>(null);
+  const [endDate, setEndDate] = useState<Moment | null>(null);
+
+  const onDateChange = (date: Date, type: "START_DATE" | "END_DATE") => {
+    if (type === "START_DATE") {
+      setStartDate(moment(date));
+    } else {
+      setEndDate(moment(date));
+    }
+  };
+
+  const onDateSelectionContinue = () => {
+    if (!startDate || !endDate) {
+      ToastAndroid.show("Please select both start and end dates", ToastAndroid.LONG);
+      return;
+    }
+    const totalNoOfDays = endDate.diff(startDate, "days");
+    console.log(totalNoOfDays);
+    setTripData({ ...tripData, startDate, endDate, totalNoOfDays: totalNoOfDays + 1 });
+  };
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: "",
+    });
+  }, []);
   return (
-    <View>
-      <Text>SelectDates</Text>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: Colors.white,
+        height: "100%",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 35,
+          fontFamily: "outfit-bold",
+          marginTop: 20,
+          textAlign: "center",
+        }}
+      >
+        Travel Dates
+      </Text>
+      <View
+        style={{
+          marginTop: 30,
+        }}
+      >
+        <CalendarPicker
+          onDateChange={onDateChange}
+          allowRangeSelection={true}
+          minDate={new Date()}
+          maxRangeDuration={5}
+          selectedRangeStyle={{
+            backgroundColor: Colors.gray,
+          }}
+          selectedDayTextStyle={{
+            color: Colors.white,
+          }}
+        />
+      </View>
+
+      <TouchableOpacity
+        onPress={onDateSelectionContinue}
+        style={{
+          padding: 15,
+          backgroundColor: Colors.primary,
+          borderRadius: 99,
+          marginTop: 20,
+          borderWidth: 1,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-regular",
+            fontSize: 17,
+            textAlign: "center",
+            color: Colors.white,
+          }}
+        >
+          Continue
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
